4e6b151206477d5cdcae45c071b827b929ef939f
romduda <romanduda@gmail.com>
2021-05-06 21:14:55 +0100

    Added tests (#1)
    feat: adds additional tests
    
    Co-authored-by: Roman Duda <roman.duda@80000hours.org>
    Co-authored-by: Ross Curry <ross_curry@yahoo.co.uk>

--- client/babel.config.js
+++ client/babel.config.js
@@ -2,5 +2,23 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-  };
+    env: {
+      development: {
+        plugins: [['inline-dotenv', {
+          path: '.env'
+        }]]
+      }
+    }
+    // transform: {
+      // "^.+\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js",
+    //   "^.+\\.tsx?$": "ts-jest",
+      // "^.+\\.[jt]sx?$": "babel-jest",
+    }
+  //   env: {
+  //     "test": {
+  //       "plugins": ["@babel/plugin-transform-modules-commonjs"]
+  //     }
 };
+
+
+
